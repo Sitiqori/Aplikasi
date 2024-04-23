8147f31ca17e16ce005d090699d253b016d8c90c
Sitiqori <96257080+Sitiqori@users.noreply.github.com>
2024-04-23 10:24:01 +0700

    Add files via upload

---
+++ perpus (1).sql
@@ -0,0 +1,529 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2024 at 03:20 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `book_code` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `cover` varchar(255) DEFAULT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `author` varchar(255) NOT NULL,
+  `publisher` varchar(255) NOT NULL,
+  `publication_year` year(4) NOT NULL,
+  `sinopsis` text DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'in stock',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `category_id` bigint(20) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `book_code`, `title`, `cover`, `slug`, `author`, `publisher`, `publication_year`, `sinopsis`, `status`, `created_at`, `updated_at`, `deleted_at`, `category_id`) VALUES
+(1, '0001', 'Harry Potter and The Goblet of Fire', 'Harry Potter and The Goblet of Fire-1712041740.jpg', 'harry-potter-and-the-goblet-of-fire', 'J.K Rowling', 'Gramedia', '2005', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus provident, consequatur debitis voluptatum fugiat magni soluta vitae dolores eos aperiam.', 'dipinjam', '2024-04-01 21:20:48', '2024-04-22 19:20:15', NULL, NULL),
+(2, '0002', 'Harry Potter and the Deathly Hallows Pt1', 'Harry Potter and the Deathly Hallows Pt1-1712042044.jpg', 'harry-potter-and-the-deathly-hallows-pt1', 'J.K Rowling', 'Gramedia', '2007', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quaerat voluptatem earum vero aut, voluptatum dignissimos esse perferendis veniam a repellat ratione minima totam quasi reprehenderit asperiores natus officiis corrupti.', 'dipinjam', '2024-04-02 00:14:04', '2024-04-22 06:09:44', NULL, NULL),
+(3, '0003', 'Harry Potter and the sorcerer\'s stone', 'Harry Potter and the sorcerer\'s stone-1712042135.jpg', 'harry-potter-and-the-sorcerer-s-stone', 'J.K Rowling', 'Gramedia', '2001', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quaerat voluptatem earum vero aut, voluptatum dignissimos esse perferendis veniam a repellat ratione minima totam quasi reprehenderit asperiores consectetur adipisicing elit. Beatae quaerat voluptatem earum vero aut, voluptatum dignissimos esse perferendis veniam a repellat ratione minima totam quasi reprehenderit asperiores natus officiis corrupti.', 'dipinjam', '2024-04-02 00:15:35', '2024-04-22 08:39:58', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_category`
+--
+
+CREATE TABLE `book_category` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `book_id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `book_category`
+--
+
+INSERT INTO `book_category` (`id`, `book_id`, `category_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, NULL, NULL),
+(4, 2, 2, NULL, NULL),
+(5, 2, 4, NULL, NULL),
+(6, 3, 2, NULL, NULL),
+(7, 3, 4, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `slug`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'comic', 'comic-data', NULL, NULL, NULL),
+(2, 'novel', 'novel-data', NULL, NULL, NULL),
+(3, 'fiction', 'fiction-data', NULL, NULL, NULL),
+(4, 'mystery', 'mystery-data', NULL, NULL, NULL),
+(5, 'horror', 'horror-data', NULL, NULL, NULL),
+(6, 'romance', 'romance-data', NULL, NULL, NULL),
+(7, 'western', 'western-data', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `koleksipribadi`
+--
+
+CREATE TABLE `koleksipribadi` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `books_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2022_10_12_043011_create_roles_table', 1),
+(6, '2022_10_12_043805_add_role_id_coloumn_to_users_table', 1),
+(7, '2022_10_12_051019_create_categories_table', 1),
+(8, '2022_10_12_051102_create_books_table', 1),
+(9, '2022_10_12_051251_create_book_category_table', 1),
+(10, '2022_10_12_051733_create_rent_logs_table', 1),
+(11, '2022_10_21_135132_add_slug_column_to_categories_table', 1),
+(12, '2022_10_21_144730_change_slug_column_into_nullable_in_categories_table', 1),
+(13, '2022_10_21_150943_add_soft_delete_column_to_categories_table', 1),
+(14, '2022_10_26_064521_add_slug_cover_column_to_books_table', 1),
+(15, '2022_10_26_151328_add_soft_delete_to_books_table', 1),
+(16, '2022_11_02_142810_add_slug_to_users_table', 1),
+(17, '2022_11_02_150423_add_softdelete_to_users_table', 1),
+(18, '2024_04_01_060550_create_ulasan_buku_table', 1),
+(19, '2024_04_01_060806_create_koleksi_pribadi_table', 1),
+(20, '2024_04_01_062042_add_categori_id_to_buku_table', 1),
+(21, '2024_04_22_132211_add_rent_log_id_to_ulasan_buku_table', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rent_logs`
+--
+
+CREATE TABLE `rent_logs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `book_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `tanggal_peminjaman` date NOT NULL,
+  `tanggal_pengembalian` date DEFAULT NULL,
+  `tanggal_harus_dikembalikan` date DEFAULT NULL,
+  `status_peminjaman` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `rent_logs`
+--
+
+INSERT INTO `rent_logs` (`id`, `book_id`, `user_id`, `tanggal_peminjaman`, `tanggal_pengembalian`, `tanggal_harus_dikembalikan`, `status_peminjaman`, `created_at`, `updated_at`) VALUES
+(1, 1, 8, '2024-04-02', '2024-04-22', '2024-04-05', 'dikembalikan', '2024-04-01 23:46:50', '2024-04-22 01:58:06'),
+(2, 2, 8, '2024-04-22', '2024-04-22', '2024-04-25', 'dikembalikan', '2024-04-22 01:59:26', '2024-04-22 02:00:03'),
+(3, 3, 8, '2024-04-22', '2024-04-22', '2024-04-25', 'dikembalikan', '2024-04-22 01:59:36', '2024-04-22 07:01:23'),
+(4, 1, 8, '2024-04-22', '2024-04-22', '2024-04-25', 'dikembalikan', '2024-04-22 01:59:42', '2024-04-22 02:00:09'),
+(5, 2, 8, '2024-04-22', NULL, '2024-04-25', 'dipinjam', '2024-04-22 06:09:43', '2024-04-22 06:09:43'),
+(6, 3, 9, '2024-04-22', NULL, '2024-04-25', 'dipinjam', '2024-04-22 08:39:57', '2024-04-22 08:39:57'),
+(7, 1, 8, '2024-04-23', NULL, '2024-04-26', 'dipinjam', '2024-04-22 19:20:14', '2024-04-22 19:20:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'admin', NULL, NULL),
+(2, 'petugas', NULL, NULL),
+(3, 'client', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ulasan_buku`
+--
+
+CREATE TABLE `ulasan_buku` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `books_id` bigint(20) UNSIGNED NOT NULL,
+  `ulasan` text NOT NULL,
+  `rating` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `rent_log_id` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `ulasan_buku`
+--
+
+INSERT INTO `ulasan_buku` (`id`, `user_id`, `books_id`, `ulasan`, `rating`, `created_at`, `updated_at`, `rent_log_id`) VALUES
+(1, 8, 3, 'good', 4, '2024-04-22 06:56:35', '2024-04-22 06:56:35', 3),
+(2, 8, 3, 'ok', 4, '2024-04-22 06:56:59', '2024-04-22 06:56:59', 3),
+(3, 8, 2, 'rr', 2, '2024-04-22 07:13:19', '2024-04-22 07:13:19', 2),
+(4, 8, 2, 'gi', 5, '2024-04-22 07:13:33', '2024-04-22 07:13:33', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'active',
+  `role_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `slug`, `name`, `password`, `email`, `address`, `status`, `role_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'admin', 'admin', 'administrator', '$2y$10$aMcbSMe1QjUx2vVx1OSR5Oddst3zNPvUuhUZvDT.lYmcEUyDCmK9a', 'admin@gmail.com', 'alamat', 'active', 1, '2024-04-01 21:02:34', '2024-04-01 21:02:34', NULL),
+(2, 'petugas', 'petugas', 'petugass', '$2y$10$vi0Aj.7Xpw4QQKPAx14mFuuGBWKkFa11e744a00DnY67KQrtIYoUa', 'petugas@gmail.com', 'Alamat', 'active', 2, '2024-04-01 21:03:30', '2024-04-01 21:03:30', NULL),
+(8, 'peminjam', 'peminjam', 'peminjam', '$2y$10$.OCOyfADm/38TMjv2C79i.cMVznifBrNcxVb0qAH4NXNM64APudmy', 'peminjam1@gmail.com', 'alamat', 'active', 3, '2024-04-01 21:04:21', '2024-04-01 21:04:21', NULL),
+(9, 'qori1', 'qori1', 'qoriii', '$2y$10$fGSL08hPiDeUUdSWvfMWxOPau9RxAXsI2P2CiLbjJwqhg7kCbxk2K', 'qori1@gmail.com', 'rumahh', 'active', 2, '2024-04-22 07:27:52', '2024-04-22 10:11:39', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `books_category_id_foreign` (`category_id`);
+
+--
+-- Indexes for table `book_category`
+--
+ALTER TABLE `book_category`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `book_category_book_id_foreign` (`book_id`),
+  ADD KEY `book_category_category_id_foreign` (`category_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `koleksipribadi_user_id_foreign` (`user_id`),
+  ADD KEY `koleksipribadi_books_id_foreign` (`books_id`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `rent_logs`
+--
+ALTER TABLE `rent_logs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rent_logs_book_id_foreign` (`book_id`),
+  ADD KEY `rent_logs_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ulasan_buku`
+--
+ALTER TABLE `ulasan_buku`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ulasan_buku_user_id_foreign` (`user_id`),
+  ADD KEY `ulasan_buku_books_id_foreign` (`books_id`),
+  ADD KEY `ulasan_buku_rent_log_id_foreign` (`rent_log_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD KEY `users_role_id_foreign` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `book_category`
+--
+ALTER TABLE `book_category`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rent_logs`
+--
+ALTER TABLE `rent_logs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ulasan_buku`
+--
+ALTER TABLE `ulasan_buku`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `book_category` (`id`);
+
+--
+-- Constraints for table `book_category`
+--
+ALTER TABLE `book_category`
+  ADD CONSTRAINT `book_category_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `book_category_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
+
+--
+-- Constraints for table `koleksipribadi`
+--
+ALTER TABLE `koleksipribadi`
+  ADD CONSTRAINT `koleksipribadi_books_id_foreign` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `koleksipribadi_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `rent_logs`
+--
+ALTER TABLE `rent_logs`
+  ADD CONSTRAINT `rent_logs_book_id_foreign` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `rent_logs_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `ulasan_buku`
+--
+ALTER TABLE `ulasan_buku`
+  ADD CONSTRAINT `ulasan_buku_books_id_foreign` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`),
+  ADD CONSTRAINT `ulasan_buku_rent_log_id_foreign` FOREIGN KEY (`rent_log_id`) REFERENCES `rent_logs` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `ulasan_buku_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_role_id_foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
